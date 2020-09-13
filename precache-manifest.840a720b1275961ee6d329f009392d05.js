29fc655bd08518277030ae357033bc6dbec601cb
Danil Stoyanov <d.stoyanov@corp.mail.ru>
2020-09-13 23:25:21 +0300

    Updates

---
+++ precache-manifest.840a720b1275961ee6d329f009392d05.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b11ba156837bcb4422b14889c5d9134",
+    "url": "/charityweb/index.html"
+  },
+  {
+    "revision": "4c1fbb286999d05518f9",
+    "url": "/charityweb/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "5900f4df96f30bf6b90b",
+    "url": "/charityweb/static/css/main.fdf0614a.chunk.css"
+  },
+  {
+    "revision": "4c1fbb286999d05518f9",
+    "url": "/charityweb/static/js/2.17281613.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/charityweb/static/js/2.17281613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5900f4df96f30bf6b90b",
+    "url": "/charityweb/static/js/main.c294921d.chunk.js"
+  },
+  {
+    "revision": "c945573bb9484e9c6992",
+    "url": "/charityweb/static/js/runtime-main.7eebc1be.js"
+  },
+  {
+    "revision": "0a18033ae614a7a971c13665ab0ed8e2",
+    "url": "/charityweb/static/media/comment.0a18033a.png"
+  },
+  {
+    "revision": "606fc12cc4be25d58a564ef056e3a725",
+    "url": "/charityweb/static/media/kitten.606fc12c.png"
+  }
+]);
